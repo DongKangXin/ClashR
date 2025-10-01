d0ff92530d7d140862076942581ea77877c9f9b3
sakura <dongkangxin@outlook.com>
2025-10-01 09:10:08 +0800

    Initial Commit

---
+++ ClashR/ClashRApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClashRApp.swift
+//  ClashR
+//
+//  Created by 董康鑫 on 2025/10/1.
+//
+
+import SwiftUI
+
+@main
+struct ClashRApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ClashR/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ClashR
+//
+//  Created by 董康鑫 on 2025/10/1.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
